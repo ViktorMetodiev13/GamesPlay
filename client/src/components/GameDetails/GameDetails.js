b83ff1076171b84d303da2603b591fcd6ce59631
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-03-31 16:09:15 +0300

    changes

--- client/src/components/GameDetails/GameDetails.js
+++ client/src/components/GameDetails/GameDetails.js
@@ -22,7 +22,7 @@ export const GameDetails = () =>
             <div className="info-section">
 
                 <div className="game-header">
-                    <img className="game-img" src={game.imageUrl} />
+                    <img className="game-img" src={game.imageUrl} alt={`${game.title} image`}/>
                     <h1>{game.title}</h1>
                     <span className="levels">MaxLevel: {game.maxLevel}</span>
                     <p className="type">{game.category}</p>
