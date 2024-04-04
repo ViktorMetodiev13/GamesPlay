6ddc68ddeedd39d1ec0c023636779f5aec153260
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-04-04 23:54:33 +0300

    AuthContext and useForm hook, login

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import { useState, useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
+import * as gameService from "./services/gameService";
+import { AuthContext } from "./contexts/AuthContext";
+
 import { Header } from "./components/Header/Header";
 import { Home } from "./components/Home/Home";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
 import { CreateGame } from "./components/CreateGame/CreateGame";
 import { Catalog } from "./components/Catalog/Catalog";
-
-import * as gameService from "./services/gameService";
 import { GameDetails } from "./components/GameDetails/GameDetails";
 
+
 function App() {
-    const [games, setGames] = useState([]);
     const navigate = useNavigate();
+    const [games, setGames] = useState([]);
+    const [auth, setAuth] = useState({});
 
     useEffect(() => {
         gameService.getAll()
@@ -31,22 +34,30 @@ function App() {
         navigate('/catalog');
     };
 
+    const onLoginSubmit = async (e) => {
+        e.preventDefault();
+
+        console.log(Object.fromEntries(new FormData(e.target)));
+    };
+
     return (
-        <div id="box">
-            <Header />
+        <AuthContext.Provider value={{onLoginSubmit}}>
+            <div id="box">
+                <Header />
 
-            <main id="main-content">
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/create-game" element={<CreateGame onCreateGameSubmit={onCreateGameSubmit} />} />
-                    <Route path="/catalog" element={<Catalog games={games}/>} />
-                    <Route path="/catalog/:gameId" element={<GameDetails />} />
-                </Routes>
-            </main>
+                <main id="main-content">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/create-game" element={<CreateGame onCreateGameSubmit={onCreateGameSubmit} />} />
+                        <Route path="/catalog" element={<Catalog games={games} />} />
+                        <Route path="/catalog/:gameId" element={<GameDetails />} />
+                    </Routes>
+                </main>
 
-        </div>
+            </div>
+        </AuthContext.Provider>
     );
 }
 
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,21 +1,45 @@
+import { useState, useContext } from "react";
+
+import { AuthContext } from "../../contexts/AuthContext";
+import { useForm } from "../../hooks/useForm";
+
 export const Login = () => {
+    const { onLoginSubmit } = useContext(AuthContext);
+    const { values, changeHandler } = useForm({
+        email: '',
+        password: '',
+    })
+
     return (
         <section id="login-page" className="auth">
-              <form id="login">
-                  <div className="container">
-                      <div className="brand-logo"></div>
-                      <h1>Login</h1>
-                      <label htmlFor="email">Email:</label>
-                      <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
-  
-                      <label htmlFor="login-pass">Password:</label>
-                      <input type="password" id="login-password" name="password" />
-                      <input type="submit" className="btn submit" value="Login" />
-                      <p className="field">
-                          <span>If you don't have profile click <a href="#">here</a></span>
-                      </p>
-                  </div>
-              </form>
-          </section>
+            <form id="login" onSubmit={onLoginSubmit}>
+                <div className="container">
+                    <div className="brand-logo"></div>
+                    <h1>Login</h1>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="Sokka@gmail.com"
+                        value={values.name}
+                        onChange={changeHandler}
+                    />
+
+                    <label htmlFor="login-pass">Password:</label>
+                    <input
+                        type="password"
+                        id="login-password"
+                        name="password"
+                        value={values.password}
+                        onChange={changeHandler}
+                    />
+                    <input type="submit" className="btn submit" value="Login" />
+                    <p className="field">
+                        <span>If you don't have profile click <a href="#">here</a></span>
+                    </p>
+                </div>
+            </form>
+        </section>
     );
 }
