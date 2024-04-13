43c677cb6525dfefe62d11d3e786537e749c05fd
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-04-13 23:59:33 +0300

    changes

--- client/src/components/Catalog/CatalogItem/CatalogItem.js
+++ client/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -12,7 +12,6 @@ export const CatalogItem = ({
                 <img src={imageUrl} />
                 <h6>{category}</h6>
                 <h2>{title}</h2>
-                <Link to={`/catalog/${_id}`} className="details-button">Details</Link>
             </div>
         </div>
     );
